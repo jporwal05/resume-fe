bca5ba69f8c1fd3d7cffbf967c5deb81b960b083
jporwal05 <jayantporwal05@gmail.com>
2023-02-22 00:13:57 +0530

    static details

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,204 @@
 <template>
-  <div class="hello">
+  <div>
     <h1 class="h-11 text-red-700">Welcome to my CV</h1>
+    <div>
+        <div class="bg-slate-400 grid grid-flow-col grid-rows-1">
+            <div class="flex flex-row items-center">
+                <p class="text-lg">Jayant Porwal</p>
+                <p class="text-xl text-gray-600">&nbsp;|&nbsp;</p>
+                <p class="text-xs">Sr. Consultant, Application Developer</p>
+            </div>
+        </div>
+        <div class="bg-slate-400 grid grid-flow-col grid-rows-1">
+            <div class="flex flex-row items-center">
+                <p class="text-xs">Email</p>
+                <p class="text-lg text-gray-600">&nbsp;|&nbsp;</p>
+                <p class="text-xs">LinkedIn</p>
+                <p class="text-lg text-gray-600">&nbsp;|&nbsp;</p>
+                <p class="text-xs">Medium</p>
+            </div>
+        </div>
+        <div class="bg-slate-400 grid grid-flow-col grid-rows-1">
+            <div class="flex flex-col">
+                <p class="text-base mt-4">Objective</p>
+                <p class="text-sm mt-1 text-justify">To work for an organization where I can contribute my skills and understand the responsibilities and
+                    strive for collective growth and development working together with my colleagues as a team, always
+                    keeping organization goals as priority.</p>
+            </div>
+        </div>
+        <div class="bg-slate-400 grid grid-flow-col grid-rows-1">
+            <div class="flex flex-col">
+                <p class="text-base mt-4">Technologies</p>
+                <p class="text-sm mt-1 text-justify">Java, Spring, Spring Boot, Hibernate, Microservices, Kibana, Grafana, PinPoint, Apache Kafka, Apache
+                    Zeppelin, Jenkins, Git, Maven, Intellij Idea, MySQL, Design, OOP, SOLID principles, Oracle, Apache
+                    ShardingSphere, Apache Camel</p>
+            </div>
+        </div>
+        <div class="bg-slate-400 grid grid-flow-col grid-rows-1">
+            <div class="flex flex-col">
+                <p class="text-base mt-4">Unique Selling Point</p>
+                <p class="text-sm mt-1 text-justify">A fast learner who can learn any modern technology at a faster pace as per project needs. A team
+                    player who always shares his knowledge so that everyone can grow together. I also have good data
+                    analytical skills.</p>
+            </div>
+        </div>
+        <div class="bg-slate-400 grid grid-flow-col grid-rows-1">
+            <div class="flex flex-col">
+                <p class="text-base mt-4">Professional Summary</p>
+                <p class="text-sm mt-1 text-justify">Backend Java developer at core who has 10+ years of industry experience in developing standalone
+                    applications as well as production ready enterprise applications to meet the business and client needs
+                    while keeping sense of latest technologies and DevOps methodologies. Someone who is not restricted
+                    by technology and open to learning any modern technology that adds value. Shares knowledge among
+                    the team to elevate everybody around and takes ownership of a given piece of work taking care of
+                    end-to-end communication, requirement gathering, design, development, testing, and postproduction
+                    support.</p>
+            </div>
+        </div>
+        <div class="bg-slate-400 grid grid-flow-col grid-rows-1">
+            <div class="flex flex-col">
+                <div>
+                    <p class="text-base mt-4">Experience</p>
+                </div>
+                <div>
+                    <div class="grid grid-flow-row grid-cols-2 mt-1">
+                        <div>
+                            <p class="text-sm">Trainee Software Engineer — HSBC</p>
+                        </div>
+                        <div>
+                            <p class="text-sm">Aug 2012 — Mar 2013</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text-sm text-justify">Built a framework using Core Java and XSLT to make the bank compliant with
+                            Dodd Frank Act for Regulatory Reporting.</p>
+                    </div>
+                </div>
+                <div>
+                    <div class="grid grid-flow-row grid-cols-2 mt-2">
+                        <div>
+                            <p class="text-sm">Software Engineer — HSBC</p>
+                        </div>
+                        <div>
+                            <p class="text-sm">Apr 2013 — Oct 2015</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text-sm text-justify">Developed a multithreaded xml comparison tool to compare trade xml(s) in
+                            bulk. Scaled the already developed reporting framework to make room for
+                            more regulations and requirements.</p>
+                    </div>
+                </div>
+                <div>
+                    <div class="grid grid-flow-row grid-cols-2 mt-2">
+                        <div>
+                            <p class="text-sm">Senior Software Engineer — HSBC</p>
+                        </div>
+                        <div>
+                            <p class="text-sm">Nov 2015 – Mar 2018</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text-sm text-justify">Travelled to London to co-ordinate the production release of a new product.
+                            Worked in writing a new project from scratch by writing proof of concepts
+                            for modern technologies including gRPC, MongoDB, ReactJS, Swagger-UI and
+                            picking up the most suited.</p>
+                    </div>
+                </div>
+                <div>
+                    <div class="grid grid-flow-row grid-cols-2 mt-2">
+                        <div>
+                            <p class="text-sm">Senior Software Analyst — Barclays</p>
+                        </div>
+                        <div>
+                            <p class="text-sm">Apr 2018 – May 2019</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text-sm text-justify">Worked on developing a high performant and stable RESTful microservice
+                            that would get trade XML(s) from a data store using REST and send them on
+                            queue with headers as per requirement using Apache Camel. Developed a
+                            field validation framework for validating fields in a Java object.</p>
+                    </div>
+                </div>
+                <div>
+                    <div class="grid grid-flow-row grid-cols-2 mt-2">
+                        <div>
+                            <p class="text-sm">Technical Lead — Worldpay from FIS</p>
+                        </div>
+                        <div>
+                            <p class="text-sm">Jun 2019 – May 2021</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text-sm text-justify">Designed and led the work on software upgrade module and auto boarding
+                            of merchants on our Integrated Payment Client commonly known as IPC. The
+                            tech stack was spring-kafka and was developed using microservice
+                            architecture.</p>
+                    </div>
+                </div>
+                <div>
+                    <div class="grid grid-flow-row grid-cols-2 mt-2">
+                        <div>
+                            <p class="text-sm">Staff Engineer — Nagarro</p>
+                        </div>
+                        <div>
+                            <p class="text-sm">May 2021 - May 2022</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text-sm text-justify">Designed two major payments/refunds related features for an Indian e-
+                            commerce giant and lead the work on writing clean and re-usable code so
+                            that the code is extensible and scalable in future. Voluntarily analyzed data
+                            from data lake and fixed many payments and refunds related issues that were
+                            existing in the system and improved customer experience significantly.</p>
+                    </div>
+                </div>
+                <div>
+                    <div class="grid grid-flow-row grid-cols-2 mt-2">
+                        <div>
+                            <p class="text-sm">Sr. Consultant, Application Developer — ThoughtWorks</p>
+                        </div>
+                        <div>
+                            <p class="text-sm">May 2022 - Present</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text-sm text-justify">Co-designed and developed a Health Campaign Management System in the 
+                            Digital Public Goods(DPG) space to allow more than 30 countries to run different Health Campaigns coherently using 
+                            a single platform. The USP of the platform is that it can work in offline mode.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bg-slate-400 grid grid-flow-col grid-rows-1">
+            <div class="flex flex-col">
+                <div>
+                    <p class="text-base mt-4">Education</p>
+                </div>
+                <div>
+                    <div class="grid grid-flow-row grid-cols-2 mt-1">
+                        <div>
+                            <p class="text-sm">Chameli Devi Group of Institutions - B.E., Indore</p>
+                        </div>
+                        <div>
+                            <p class="text-sm">2012</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text-xs text-justify">Computer Science and Engineering - 70% aggregate</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bg-slate-400 grid grid-flow-col grid-rows-1">
+            <div class="flex flex-col">
+                <p class="text-base mt-4">Communication</p>
+                <p class="text-sm mt-1 text-justify">I can deliver crisp and clear presentations; may it be design proposals or solution proposals. I like to
+                    document as much as possible so that it is easy to share knowledge, root cause analysis or back track.</p>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
