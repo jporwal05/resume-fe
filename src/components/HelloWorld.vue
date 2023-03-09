a96653937bbe7cb5cde840323189903bc44eeb39
jporwal05 <jayantporwal05@gmail.com>
2023-03-10 01:40:44 +0530

    font and font size changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -21,29 +21,29 @@
         <div class="grid grid-flow-col grid-rows-1 mt-1">
           <div class="flex flex-row items-center flex-wrap">
             <p class="text-sm font-semibold">jayantporwal05@gmail.com</p>
-            <p class="text-lg text-gray-600">&nbsp;&nbsp;</p>
+            <p class="text-lg">&nbsp;&nbsp;</p>
             <a
               class="text-sm font-semibold underline 
                             hover:underline-offset-2 hover:decoration-blue-500 
                             transition delay-150"
               href="https://www.linkedin.com/in/jporwal05/"
             >LinkedIn</a>
-            <p class="text-lg text-gray-600">&nbsp;&nbsp;</p>
+            <p class="text-lg">&nbsp;&nbsp;</p>
             <a
               class="text-sm font-semibold underline 
                             hover:underline-offset-2 hover:decoration-blue-500 
                             transition delay-150"
               href="https://medium.com/@jayantporwal05"
             >Medium</a>
-            <p class="text-lg text-gray-600">&nbsp;&nbsp;</p>
+            <p class="text-lg">&nbsp;&nbsp;</p>
             <p class="text-sm font-semibold">+91-8698033662</p>
           </div>
         </div>
         <div class="grid grid-flow-col grid-rows-1 mt-2">
           <div class="flex flex-row items-center flex-wrap">
-            <p class="text-sm font-semibold border border-red-500 rounded px-2 hover:bg-red-300 transition delay-150 animate-bounce">Remote</p>
+            <p class="text-sm font-semibold border border-blue-500 rounded px-2 hover:bg-blue-300 transition delay-150 animate-bounce">Remote</p>
             <p class="text-sm font-semibold">&nbsp;or&nbsp;</p>
-            <p class="text-sm font-semibold border border-red-500 rounded px-2 hover:bg-red-300 transition delay-150 animate-bounce">Indore, India</p>
+            <p class="text-sm font-semibold border border-blue-500 rounded px-2 hover:bg-blue-300 transition delay-150 animate-bounce">Indore, India</p>
             <p class="text-sm font-semibold">&nbsp;only&nbsp;</p>
           </div>
         </div>
@@ -51,7 +51,7 @@
       <div class="grid grid-flow-col grid-rows-1 p-4">
         <div class="flex flex-col">
           <p class="text-4xl font-bold mt-4">Objective</p>
-          <p class="text-xl font-medium mt-1 text-justify">To work for an organization where I can contribute my skills and understand the responsibilities and
+          <p class="text-lg mt-1 text-justify">To work for an organization where I can contribute my skills and understand the responsibilities and
             strive for collective growth and development working together with my colleagues as a team, always
             keeping organization goals as priority.</p>
         </div>
@@ -59,7 +59,7 @@
       <div class="grid grid-flow-col grid-rows-1 p-4">
         <div class="flex flex-col">
           <p class="text-4xl font-bold mt-4">Technologies</p>
-          <p class="text-xl font-medium mt-1 text-justify">Java, Spring, Spring Boot, Hibernate, Microservices, Kibana, Grafana, PinPoint, Apache Kafka, Apache
+          <p class="text-lg mt-1 text-justify">Java, Spring, Spring Boot, Hibernate, Microservices, Kibana, Grafana, PinPoint, Apache Kafka, Apache
             Zeppelin, Jenkins, Git, Maven, Intellij Idea, MySQL, Design, OOP, SOLID principles, Oracle, Apache
             ShardingSphere, Apache Camel</p>
         </div>
@@ -67,7 +67,7 @@
       <div class="grid grid-flow-col grid-rows-1 p-4">
         <div class="flex flex-col">
           <p class="text-4xl font-bold mt-4">Unique Selling Point</p>
-          <p class="text-xl font-medium mt-1 text-justify">A fast learner who can learn any modern technology at a faster pace as per project needs. A team
+          <p class="text-lg mt-1 text-justify">A fast learner who can learn any modern technology at a faster pace as per project needs. A team
             player who always shares his knowledge so that everyone can grow together. I also have good data
             analytical skills.</p>
         </div>
@@ -75,7 +75,7 @@
       <div class="grid grid-flow-col grid-rows-1 p-4">
         <div class="flex flex-col">
           <p class="text-4xl font-bold mt-4">Professional Summary</p>
-          <p class="text-xl font-medium mt-1 text-justify">Backend Java developer at core who has 10+ years of industry experience in developing standalone
+          <p class="text-lg mt-1 text-justify">Backend Java developer at core who has 10+ years of industry experience in developing standalone
             applications as well as production ready enterprise applications to meet the business and client needs
             while keeping sense of latest technologies and DevOps methodologies. Someone who is not restricted
             by technology and open to learning any modern technology that adds value. Shares knowledge among
@@ -90,93 +90,93 @@
             <p class="text-4xl font-bold mt-4">Experience</p>
           </div>
           <div>
-            <div class="grid grid-flow-row grid-cols-2 mt-1 font-semibold text-gray-700 items-center">
+            <div class="grid grid-flow-row grid-cols-2 mt-1 font-semibold text-blue-500 items-center">
               <div>
-                <p class="text-xl mt-2">Trainee Software Engineer — HSBC</p>
+                <p class="text-sm mt-2">Trainee Software Engineer — HSBC</p>
               </div>
               <div>
                 <p class="text-sm text-center">Aug 2012 — Mar 2013</p>
               </div>
             </div>
             <div>
-              <p class="text-xl font-medium text-justify">Built a framework using Core Java and XSLT to make the bank compliant with
+              <p class="text-lg text-justify">Built a framework using Core Java and XSLT to make the bank compliant with
                 Dodd Frank Act for Regulatory Reporting.</p>
             </div>
           </div>
           <div>
-            <div class="grid grid-flow-row grid-cols-2 mt-2 font-semibold text-gray-700 items-center">
+            <div class="grid grid-flow-row grid-cols-2 mt-2 font-semibold text-blue-500 items-center">
               <div>
-                <p class="text-xl mt-2">Software Engineer — HSBC</p>
+                <p class="text-sm mt-2">Software Engineer — HSBC</p>
               </div>
               <div>
                 <p class="text-sm text-center">Apr 2013 — Oct 2015</p>
               </div>
             </div>
             <div>
-              <p class="text-xl font-medium text-justify">Developed a multithreaded xml comparison tool to compare trade xml(s) in
+              <p class="text-lg text-justify">Developed a multithreaded xml comparison tool to compare trade xml(s) in
                 bulk. Scaled the already developed reporting framework to make room for
                 more regulations and requirements.</p>
             </div>
           </div>
           <div>
-            <div class="grid grid-flow-row grid-cols-2 mt-2 font-semibold text-gray-700 items-center">
+            <div class="grid grid-flow-row grid-cols-2 mt-2 font-semibold text-blue-500 items-center">
               <div>
-                <p class="text-xl mt-2">Senior Software Engineer — HSBC</p>
+                <p class="text-sm mt-2">Senior Software Engineer — HSBC</p>
               </div>
               <div>
                 <p class="text-sm text-center">Nov 2015 – Mar 2018</p>
               </div>
             </div>
             <div>
-              <p class="text-xl font-medium text-justify">Travelled to London to co-ordinate the production release of a new product.
+              <p class="text-lg text-justify">Travelled to London to co-ordinate the production release of a new product.
                 Worked in writing a new project from scratch by writing proof of concepts
                 for modern technologies including gRPC, MongoDB, ReactJS, Swagger-UI and
                 picking up the most suited.</p>
             </div>
           </div>
           <div>
-            <div class="grid grid-flow-row grid-cols-2 mt-2 font-semibold text-gray-700 items-center">
+            <div class="grid grid-flow-row grid-cols-2 mt-2 font-semibold text-blue-500 items-center">
               <div>
-                <p class="text-xl mt-2">Senior Software Analyst — Barclays</p>
+                <p class="text-sm mt-2">Senior Software Analyst — Barclays</p>
               </div>
               <div>
                 <p class="text-sm text-center">Apr 2018 – May 2019</p>
               </div>
             </div>
             <div>
-              <p class="text-xl font-medium text-justify">Worked on developing a high performant and stable RESTful microservice
+              <p class="text-lg text-justify">Worked on developing a high performant and stable RESTful microservice
                 that would get trade XML(s) from a data store using REST and send them on
                 queue with headers as per requirement using Apache Camel. Developed a
                 field validation framework for validating fields in a Java object.</p>
             </div>
           </div>
           <div>
-            <div class="grid grid-flow-row grid-cols-2 mt-2 font-semibold text-gray-700 items-center">
+            <div class="grid grid-flow-row grid-cols-2 mt-2 font-semibold text-blue-500 items-center">
               <div>
-                <p class="text-xl mt-2">Technical Lead — Worldpay from FIS</p>
+                <p class="text-sm mt-2">Technical Lead — Worldpay from FIS</p>
               </div>
               <div>
                 <p class="text-sm text-center">Jun 2019 – May 2021</p>
               </div>
             </div>
             <div>
-              <p class="text-xl font-medium text-justify">Designed and led the work on software upgrade module and auto boarding
+              <p class="text-lg text-justify">Designed and led the work on software upgrade module and auto boarding
                 of merchants on our Integrated Payment Client commonly known as IPC. The
                 tech stack was spring-kafka and was developed using microservice
                 architecture.</p>
             </div>
           </div>
           <div>
-            <div class="grid grid-flow-row grid-cols-2 mt-2 font-semibold text-gray-700 items-center">
+            <div class="grid grid-flow-row grid-cols-2 mt-2 font-semibold text-blue-500 items-center">
               <div>
-                <p class="text-xl mt-2">Staff Engineer — Nagarro</p>
+                <p class="text-sm mt-2">Staff Engineer — Nagarro</p>
               </div>
               <div>
                 <p class="text-sm text-center">May 2021 - May 2022</p>
               </div>
             </div>
             <div>
-              <p class="text-xl font-medium text-justify">Designed two major payments/refunds related features for an Indian e-
+              <p class="text-lg text-justify">Designed two major payments/refunds related features for an Indian e-
                 commerce giant and lead the work on writing clean and re-usable code so
                 that the code is extensible and scalable in future. Voluntarily analyzed data
                 from data lake and fixed many payments and refunds related issues that were
@@ -184,16 +184,16 @@
             </div>
           </div>
           <div>
-            <div class="grid grid-flow-row grid-cols-2 mt-2 font-semibold text-gray-700 items-center">
+            <div class="grid grid-flow-row grid-cols-2 mt-2 font-semibold text-blue-500 items-center">
               <div>
-                <p class="text-xl mt-2">Sr. Consultant, Developer — ThoughtWorks</p>
+                <p class="text-sm mt-2">Sr. Consultant, Developer — ThoughtWorks</p>
               </div>
               <div>
                 <p class="text-sm text-center">May 2022 - Present</p>
               </div>
             </div>
             <div>
-              <p class="text-xl font-medium text-justify">Co-designed and developed a Health Campaign Management System for a client in the
+              <p class="text-lg text-justify">Co-designed and developed a Health Campaign Management System for a client in the
                 Digital Public Goods(DPG) space to allow more than 30 countries to run different Health Campaigns coherently using
                 a single platform. The USP of the platform is that it can work in offline mode.
               </p>
@@ -207,24 +207,24 @@
             <p class="text-4xl font-bold mt-4">Education</p>
           </div>
           <div>
-            <div class="grid grid-flow-row grid-cols-2 mt-1 font-semibold text-gray-700 items-center">
+            <div class="grid grid-flow-row grid-cols-2 mt-1 font-semibold text-blue-500 items-center">
               <div>
-                <p class="text-xl">Chameli Devi Group of Institutions</p>
+                <p class="text-sm">Chameli Devi Group of Institutions</p>
               </div>
               <div>
                 <p class="text-sm text-center">2012</p>
               </div>
             </div>
-            <div class="grid grid-flow-row grid-cols-2 mt-1 font-semibold text-gray-700 items-center">
+            <div class="grid grid-flow-row grid-cols-2 mt-1 font-semibold text-blue-500 items-center">
               <div>
-                <p class="text-xl">B.E. - Computer Science and Engineering</p>
+                <p class="text-sm">B.E. - Computer Science and Engineering</p>
               </div>
               <div>
                 <p class="text-sm text-center">CGPA 7.0</p>
               </div>
             </div>
             <div>
-              <p class="text-xl text-justify">Indore, Madhya Pradesh, India</p>
+              <p class="text-sm text-justify">Indore, Madhya Pradesh, India</p>
             </div>
           </div>
         </div>
@@ -232,7 +232,7 @@
       <div class="grid grid-flow-col grid-rows-1 p-4">
         <div class="flex flex-col">
           <p class="text-4xl font-bold mt-4">Communication</p>
-          <p class="text-xl font-medium mt-1 text-justify">I can deliver crisp and clear presentations; may it be design proposals or solution proposals. I like to
+          <p class="text-lg mt-1 text-justify">I can deliver crisp and clear presentations; may it be design proposals or solution proposals. I like to
             document as much as possible so that it is easy to share knowledge, do root cause analysis or back track.</p>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
       },
     },
     fontFamily: {
-      'mono': ['"Courier Prime"', 'monospace'],
+      'mono': ['"Roboto Mono"', 'monospace'],
     },
   },
   plugins: [],
